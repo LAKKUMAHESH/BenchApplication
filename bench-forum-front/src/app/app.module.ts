11d7e4eb7e458875aa811f40d63ba183692daa75
Devaraj <d.chrnnur@gmail.com>
2019-12-18 22:11:27 +0530

    angular commit

--- bench-forum-front/src/app/app.module.ts
+++ bench-forum-front/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
